cb5224c43aefd02e835f683a76635e5cda943ff5
marcmis979 <72617994+marcmis979@users.noreply.github.com>
2022-06-18 21:51:31 +0200

    Add files via upload

---
+++ Gra.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraLanXO
+{
+    public partial class Gra : Form
+    {
+        public void PrzygotujPlansze()
+        {
+            foreach (Button btn in Controls.OfType<Button>())
+            {
+                btn.Text = "";
+            }
+
+        }
+        public Gra(bool isHost, string ip = null)
+        {
+            InitializeComponent();
+            PrzygotujPlansze();
+            MessageReceiver.DoWork += MessageReceiver_DoWork;
+            CheckForIllegalCrossThreadCalls = false;
+
+            if (isHost)
+            {
+                PlayerChar = 'X';
+                OpponentChar = 'O';
+                server = new TcpListener(System.Net.IPAddress.Any, 5732);
+                server.Start();
+                sock = server.AcceptSocket();
+                label1.Text = "Your Trun!";
+            }
+            else
+            {
+                PlayerChar = 'O';
+                OpponentChar = 'X';
+                try
+                {
+                    client = new TcpClient(ip, 5732);
+                    sock = client.Client;
+                    MessageReceiver.RunWorkerAsync();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
+        }
+
+        private void MessageReceiver_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (CheckState()) 
+            { 
+                return;
+            }
+            FreezeBoard();
+            label1.Text = "Opponent's Turn!";
+            ReceiveMove();
+            label1.Text = "Your Turn!";
+            if (!CheckState())
+                UnfreezeBoard();
+        }
+
+        private char PlayerChar;
+        private char OpponentChar;
+        private Socket sock;
+        private BackgroundWorker MessageReceiver = new BackgroundWorker();
+        private TcpListener server = null;
+        private TcpClient client;
+
+        public void Kolor(Button sender)
+        {
+            Button ctrl = ((Button)sender);
+            if (ctrl.Text=="X")
+            {
+                
+                ctrl.BackColor = System.Drawing.Color.Red; ;
+            }
+            else if (ctrl.Text=="O")
+            {
+                ctrl.BackColor = System.Drawing.Color.Blue;
+            }
+            
+        }
+        private bool CheckState()
+        {
+            //Horizontals
+            if (button1.Text == button2.Text && button2.Text == button3.Text && button3.Text != "")
+            {
+                if (button1.Text[0] == PlayerChar)
+                {
+                    label1.Text = "You Won!";
+                    MessageBox.Show("You Won!");
+                    FreezeBoard(); 
+                }
+                else
+                {
+                    label1.Text = "You Lost!";
+                    MessageBox.Show("You Lost!");
+                }
+                return true;
+            }
+
+            else if (button4.Text == button5.Text && button5.Text == button6.Text && button6.Text != "")
+            {
+                if (button4.Text[0] == PlayerChar)
+                {
+                    label1.Text = "You Won!";
+                    MessageBox.Show("You Won!");
+                    FreezeBoard();
+                }
+
+                else
+                {
+                    label1.Text = "You Lost!";
+                    MessageBox.Show("You Lost!");
+                }
+                return true;
+            }
+
+            else if (button7.Text == button8.Text && button8.Text == button9.Text && button9.Text != "")
+            {
+                if (button7.Text[0] == PlayerChar)
+                {
+                    label1.Text = "You Won!";
+                    MessageBox.Show("You Won!");
+                    FreezeBoard();
+                }
+                else
+                {
+                    label1.Text = "You Lost!";
+                    MessageBox.Show("You Lost!");
+                }
+                return true;
+            }
+
+            //Verticals
+            else if (button1.Text == button4.Text && button4.Text == button7.Text && button7.Text != "")
+            {
+                if (button1.Text[0] == PlayerChar)
+                {
+                    label1.Text = "You Won!";
+                    MessageBox.Show("You Won!");
+                    FreezeBoard();
+                }
+                else
+                {
+                    label1.Text = "You Lost!";
+                    MessageBox.Show("You Lost!");
+                }
+                return true;
+            }
+
+            else if (button2.Text == button5.Text && button5.Text == button8.Text && button8.Text != "")
+            {
+                if (button2.Text[0] == PlayerChar)
+                {
+                    label1.Text = "You Won!";
+                    MessageBox.Show("You Won!");
+                    FreezeBoard();
+                }
+                else
+                {
+                    label1.Text = "You Lost!";
+                    MessageBox.Show("You Lost!");
+                }
+                return true;
+            }
+
+            else if (button3.Text == button6.Text && button6.Text == button9.Text && button9.Text != "")
+            {
+                if (button3.Text[0] == PlayerChar)
+                {
+                    label1.Text = "You Won!";
+                    MessageBox.Show("You Won!");
+                    FreezeBoard();
+                }
+                else
+                {
+                    label1.Text = "You Lost!";
+                    MessageBox.Show("You Lost!");
+                }
+                return true;
+            }
+
+            else if (button1.Text == button5.Text && button5.Text == button9.Text && button9.Text != "")
+            {
+                if (button1.Text[0] == PlayerChar)
+                {
+                    label1.Text = "You Won!";
+                    MessageBox.Show("You Won!");
+                    FreezeBoard();
+                }
+                else
+                {
+                    label1.Text = "You Lost!";
+                    MessageBox.Show("You Lost!");
+                }
+                return true;
+            }
+
+            else if (button3.Text == button5.Text && button5.Text == button7.Text && button7.Text != "")
+            {
+                if (button3.Text[0] == PlayerChar)
+                {
+                    label1.Text = "You Won!";
+                    MessageBox.Show("You Won!");
+                    FreezeBoard();
+                }
+                else
+                {
+                    label1.Text = "You Lost!";
+                    MessageBox.Show("You Lost!");
+                }
+                return true;
+            }
+
+            //Draw
+            else if (button1.Text != "" && button2.Text != "" && button3.Text != "" && button4.Text != "" && button5.Text != "" && button6.Text != "" && button7.Text != "" && button8.Text != "" && button9.Text != "")
+            {
+                label1.Text = "It's a draw!";
+                MessageBox.Show("It's a draw!");
+                FreezeBoard();
+                return true;
+            }
+            return false;
+        }
+        private void FreezeBoard()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+        }
+
+        private void UnfreezeBoard()
+        {
+            if (button1.Text == "")
+                button1.Enabled = true;
+            if (button2.Text == "")
+                button2.Enabled = true;
+            if (button3.Text == "")
+                button3.Enabled = true;
+            if (button4.Text == "")
+                button4.Enabled = true;
+            if (button5.Text == "")
+                button5.Enabled = true;
+            if (button6.Text == "")
+                button6.Enabled = true;
+            if (button7.Text == "")
+                button7.Enabled = true;
+            if (button8.Text == "")
+                button8.Enabled = true;
+            if (button9.Text == "")
+                button9.Enabled = true;
+        }
+
+        private void ReceiveMove()
+        {
+            byte[] buffer = new byte[1];
+            sock.Receive(buffer);
+            if (buffer[0] == 1)
+                button1.Text = OpponentChar.ToString();
+            if (buffer[0] == 2)
+                button2.Text = OpponentChar.ToString();
+            if (buffer[0] == 3)
+                button3.Text = OpponentChar.ToString();
+            if (buffer[0] == 4)
+                button4.Text = OpponentChar.ToString();
+            if (buffer[0] == 5)
+                button5.Text = OpponentChar.ToString();
+            if (buffer[0] == 6)
+                button6.Text = OpponentChar.ToString();
+            if (buffer[0] == 7)
+                button7.Text = OpponentChar.ToString();
+            if (buffer[0] == 8)
+                button8.Text = OpponentChar.ToString();
+            if (buffer[0] == 9)
+                button9.Text = OpponentChar.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] num = { 1 };
+            sock.Send(num);
+            button1.Text = PlayerChar.ToString();
+            MessageReceiver.RunWorkerAsync();
+            Kolor((Button)sender);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] num = { 2 };
+            sock.Send(num);
+            button2.Text = PlayerChar.ToString();
+            MessageReceiver.RunWorkerAsync();
+            Kolor((Button)sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] num = { 3 };
+            sock.Send(num);
+            button3.Text = PlayerChar.ToString();
+            MessageReceiver.RunWorkerAsync();
+            Kolor((Button)sender);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            byte[] num = { 4 };
+            sock.Send(num);
+            button4.Text = PlayerChar.ToString();
+            MessageReceiver.RunWorkerAsync();
+            this.BackColor = System.Drawing.Color.Blue;
+            Kolor((Button)sender);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            byte[] num = { 5 };
+            sock.Send(num);
+            button5.Text = PlayerChar.ToString();
+            MessageReceiver.RunWorkerAsync();
+            Kolor((Button)sender);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            byte[] num = { 6 };
+            sock.Send(num);
+            button6.Text = PlayerChar.ToString();
+            MessageReceiver.RunWorkerAsync();
+            Kolor((Button)sender);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            byte[] num = { 7 };
+            sock.Send(num);
+            button7.Text = PlayerChar.ToString();
+            MessageReceiver.RunWorkerAsync();
+            Kolor((Button)sender);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            byte[] num = { 8 };
+            sock.Send(num);
+            button8.Text = PlayerChar.ToString();
+            MessageReceiver.RunWorkerAsync();
+            Kolor((Button)sender);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            byte[] num = { 9 };
+            sock.Send(num);
+            button9.Text = PlayerChar.ToString();
+            MessageReceiver.RunWorkerAsync();
+            Kolor((Button)sender);
+        }
+
+        private void Gra_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageReceiver.WorkerSupportsCancellation = true;
+            MessageReceiver.CancelAsync();
+            if (server != null)
+                server.Stop();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            PrzygotujPlansze();
+        }
+
+        private void Gra_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Gra_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraLanXO
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Gra newGame = new Gra(false, textBox1.Text);
+            Visible = false;
+            if (!newGame.IsDisposed)
+                newGame.ShowDialog();
+            Visible = true;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Gra newGame = new Gra(true);
+            Visible = false;
+            if (!newGame.IsDisposed)
+                newGame.ShowDialog();
+            Visible = true;
+        }
+    }
+}
